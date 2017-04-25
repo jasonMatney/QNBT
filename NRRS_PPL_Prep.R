0ccb0e5ea5264513cd4db0f0bd0d742e602bb44e
Jason Matney <jamatney@gmail.com>
2017-04-25 13:30:49 -0400

    Update NRRS_PPL_Prep.R

--- NRRS_PPL_Prep.R
+++ NRRS_PPL_Prep.R
@@ -22,7 +22,7 @@ PPL_3 <- read.csv('PPL_binned\\PPL_3.csv')
 # read in NNRS data
 dir2 <- "C:\\Users\\jamatney\\Desktop\\PhD\\NRRS_Summarized_byPark_Shapefile"
 ogrListLayers(dir2)
-NRRS <- readOGR(dsn=dir2 ,layer="NRRS_byPark_fromPython")
+
 
 # Load the package
 year_list <- c(2008:2014)
@@ -32,7 +32,7 @@ bin_list <- list(PPL_1,PPL_2,PPL_3)
 # Loop through each year of each binned data frame and write csv
 for(i in 1:length(bin_list)){
   PPL_bin <- bin_list[[i]]
-  
+  NRRS <- readOGR(dsn=dir2 ,layer="NRRS_byPark_fromPython")
   PPL_bin_table <- as.data.frame(table(PPL_bin$Park))
   names(PPL_bin_table) <- c("Park", "Freq")
   NRRS_dat_select <- sqldf("SELECT NRRS_dat.Park,
@@ -51,10 +51,9 @@ for(i in 1:length(bin_list)){
                              ON NRRS_dat.Park = PPL_bin_table.Park")
     
   NRRS@data <- cbind(NRRS@data,NRRS_dat_select[,ncol(NRRS_dat_select)])
-  names(NRRS@data)[names(NRRS@data) == "NRRS_dat_select[, ncol(NRRS_dat_select)]"] <- paste0("Frq_",i)
+  names(NRRS@data)[names(NRRS@data) == "NRRS_dat_select[, ncol(NRRS_dat_select)]"] <- paste0("PPL_Freq")
   print(paste0("writing file: ", paste0("NRRS_PPL_",i)))
   writeOGR(NRRS, dsn=dir, paste0("NRRS_PPL\\NRRS_PPL_",i), driver="ESRI Shapefile")
-  NRRS <- readOGR(dsn=dir2 ,layer="NRRS_byPark_fromPython")
 }
 
 # INDIVIDUAL YEARS (NEEDS WORK)
@@ -89,3 +88,7 @@ for(i in 1:length(bin_list)){
 #   print(paste0("writing file: ", paste0("NRRS_PPL_",i)))
 #   writeOGR(NRRS, dsn=dir, paste0("NRRS_PPL\\NRRS_PPL_",i), driver="ESRI Shapefile")
 # }
+
+
+
+
